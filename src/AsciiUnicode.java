30f8d72cc5422099ff844d26b14e32b65d39cad5
juyoung <juyoungjeong.797@gmail.com>
2025-06-05 15:54:14 +0900

    feat: Add new feature AsciiUnicode

---
+++ src/AsciiUnicode.java
@@ -0,0 +1,21 @@
+public class AsciiUnicode {
+    public static void run() {
+        char asciiCharA = 'A';
+        System.out.println("character : " + asciiCharA);
+        char asciiChara = 'a';
+        System.out.println("character : " + asciiChara);
+        char asciiChar0 = '0';
+        System.out.println("character : " + asciiChar0);
+        char asciiChar = '!';
+        System.out.println("character : " + asciiChar);
+
+        int asciiCodeA = (int) asciiCharA;
+        System.out.println("asciiCode : " + asciiCodeA);
+        int asciiCodea = (int) asciiChara;
+        System.out.println("asciiCode : " + asciiCodea);
+        int asciiCode0 = (int) asciiChar0;
+        System.out.println("asciiCode : " + asciiCode0);
+        int asciiCode = (int) asciiChar;
+        System.out.println("asciiCode : " + asciiCode);
+    }
+}
